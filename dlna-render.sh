02d880f31730e0a499ffa485390a8c3a210857b2
Zhiqiang Zhang <i@zhangzqs.cn>
2025-08-05 00:08:53 +0800

    Update dlna-render.sh

--- dlna-render.sh
+++ dlna-render.sh
@@ -4,4 +4,4 @@ if [ -z "$UPNP_DEVICE_NAME" ]; then
  UPNP_DEVICE_NAME="DLNA-Render-$(hostname)"
 fi
 
-/usr/bin/gmediarender -f "$UPNP_DEVICE_NAME" --logfile=stdout
+gmediarender -f "$UPNP_DEVICE_NAME" --logfile=stdout
